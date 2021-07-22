9492ff20cc5edfd106b0a0c59dfe37869adc2575
Shawn <19040010@rp.edu.sg>
2021-07-22 10:30:12 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/webservices/demomusicplayer/MainActivity.java
@@ -0,0 +1,16 @@
+package sg.edu.rp.webservices.demomusicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
